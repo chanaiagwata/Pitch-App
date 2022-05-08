9f58e3a27cbe0cf1ccc50c38b49ddb013eee2e36
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-05-08 19:55:02 +0300

    feat:creates pitch and categories models

--- app/models.py
+++ app/models.py
@@ -4,6 +4,8 @@ class User(db.Model):
     __tablename__  = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True, index =True)
+    password_hash=db.Column(db.String(255))
     
     def __repr__(self):
         return f'User {self.username}'
@@ -23,4 +25,17 @@ class Category(db.Model):
     def get_categories(cls,id):
         categories = Category.query.all()
         return categories
+
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
     
+    id = db.Column(db.Integer,primary_key = True)
+    gist = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  #references primary key in user table
+    categories_id = db.Column(db.Integer, db.ForeignKey('categories.id')) #references primary key in categories table
+    vote = db.relationship("Votes", backref = "pitches", lazy = "dynamic")
+    comment = db.relationship("Comments", backref = "pitches", lazy = "dynamic")
+    
+class Comments(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key = True)
--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User #importing User class from models.py
+from app.models import Category, Pitch, User, Comments #importing User class from models.py
 
 #creating app instance
 
@@ -13,7 +13,7 @@ manager.add_command('server', Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app, db = db, User = User)
+    return dict(app = app, db = db, User = User, Category = Category, Pitch = Pitch, Comments = Comments)
     pass
 
 if __name__ == '__main__':
