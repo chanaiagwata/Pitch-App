df265e440f95f0cf5e54c1dc38677ecd517a6692
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-05-09 23:19:06 +0300

    feat:will create votes class and add columns to other classes

--- app/models.py
+++ app/models.py
@@ -1,18 +1,28 @@
 from . import db
+from datetime import datetime
 
+
+#users
 class User(db.Model):
     __tablename__  = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True, index =True)
     password_hash=db.Column(db.String(255))
+    password_secure = db.Column(db.string(255))
+    pitches = db.relationship("Pitch", bakcref ="user", lazy = "dynamic")
+    vote = db.relationship("Votes", backref="user", lazy = "dynamic")
+    comment =db.relationship("Comments", backref = "user", lazy = "dynamic")
     
     def __repr__(self):
         return f'User {self.username}'
     
+    #securing our passwords
+
+#Categories
 class Category(db.Model):
-    __tablename__  ='categories'
-    id = db.Column(db.Integer, primary_Key = True)
+    __tablename__  = 'categories'
+    id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(255))
     description = db.Column(db.String(255))
    
@@ -22,20 +32,37 @@ class Category(db.Model):
         db.session.commit()
     #class method that retrieves a particular category of pitches from the Category model 
     @classmethod
-    def get_categories(cls,id):
+    def get_categories(cls):
         categories = Category.query.all()
         return categories
 
+
+#Pitches
 class Pitch(db.Model):
     __tablename__ = 'pitches'
     
     id = db.Column(db.Integer,primary_key = True)
     gist = db.Column(db.String)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  #references primary key in user table
     categories_id = db.Column(db.Integer, db.ForeignKey('categories.id')) #references primary key in categories table
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))  #references primary key in user table
     vote = db.relationship("Votes", backref = "pitches", lazy = "dynamic")
     comment = db.relationship("Comments", backref = "pitches", lazy = "dynamic")
+
+#Votes
+class Votes(db.Model):
+    __tablename__ = 'votes'
     
+    id = db.Column(db.Integer, primary_key = True)
+    vote = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    pitches_id = db.Column(db.Integer, db.ForeignKey("pitches.id"))
+
+
+#Comments
 class Comments(db.Model):
     __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key = True)
+    remark = db.Column(db.String(255))
+    time_stamp = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    pitches_id = db.Column(db.Integer, db.ForeignKey("pitches.id"))
--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import Category, Pitch, User, Comments #importing User class from models.py
+from app.models import User, Category, Pitch, Comments #importing User class from models.py
+from  flask_migrate import Migrate, MigrateCommand
 
 #creating app instance
 
@@ -9,6 +10,9 @@ app = create_app('development')
 manager = Manager(app)
 manager.add_command('server', Server)
 
+
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 #use shell decorator to create shell context and function for passing properties into shell
 
 @manager.shell
