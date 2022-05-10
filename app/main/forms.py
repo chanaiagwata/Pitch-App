76a0dfc35cdbac835e98f5244736e5fbb5a5c08f
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-05-10 20:29:37 +0300

    feat:create wtf form classes

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import Required
@@ -5,4 +6,12 @@ from wtforms.validators import Required
 
 class Pitch_form(FlaskForm):
     Pitch_form = TextAreaField('Enter new pitch')
+    submit = SubmitField('Submit')
+    
+class Category_form(FlaskForm):
+    name = StringField('Pitch category', validators=[Required()])
+    submit = SubmitField('Add to Category')
+    
+class Comment_form(FlaskForm):
+    remark = TextAreaField('Leave a comment')
     submit = SubmitField('Submit')
